c015b3968acf5d6b318835df38963d40aac97360
David Elliott <elliott2081+github@gmail.com>
2025-05-28 06:08:17 +0000

    4

--- api/index.py
+++ api/index.py
@@ -1,7 +1,7 @@
 import os
 from flask import Flask, render_template
 
-app = Flask(__name__, template_folder=os.path.abspath('templates'))
+app = Flask(__name__, template_folder='templates')
 
 @app.route('/')
 def home():
